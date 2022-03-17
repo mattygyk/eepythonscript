3866571ae0a9718facf6a890288cf0435386defe
mattyg <matthew.1.gray@co-operativebank.co.uk>
2022-03-17 17:17:56 +0000

    going well

--- createinvocationdb.py
+++ createinvocationdb.py
@@ -3,6 +3,9 @@
 import sqlite3
 con = sqlite3.connect('invocations.db')
 cur = con.cursor()
-cur.execute('''CREATE TABLE invocations (gist texdt, date text)''')
+#cur.execute('''CREATE TABLE invocations (gist text, date text)''')
+#cur.execute("INSERT INTO invocations VALUES ('mattygyk','2022-03-17')")
+cur.execute("SELECT * FROM invocations WHERE gist = 'mattygyk' ")
+print(cur.fetchall())
 con.commit()
 con.close()
--- eegistpython.py
+++ eegistpython.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 import requests
 import json
-#api_url = "https://api.github.com/gists/public"
-headers = {"Accept" : "application/json"}
-for api_url in ['https://api.github.com/gists/public']:
-    try:
-        response = requests.get(api_url, headers=headers)
-        response.text
-        data = response.json()
-        print(data)
-    except: 
-        print("failed")
-    else:
-        print(response.status_code)
+import sqlite3
+
+def db_function(gistuser):
+          con = sqlite3.connect('invocations.db')
+          con.row_factory = sqlite3.Row
+          cur = con.cursor()
+          #query db for previious run of gist user
+          cur.execute("SELECT * FROM invocations WHERE gist = 'mabongogyk' ")
+          gistdata = cur.fetchone()
+          print (gistdata)
+          if gistdata == "None" :
+              print ("Database Empty for: ", gistuser )
+          else:
+              print ("Gistuser is:", gistuser )
+
+mattty = db_function("mattygyk")
+
+# #api_url = "https://api.github.com/gists/public"
+# headers = {"Accept" : "application/json"}
+# for api_url in ['https://api.github.com/gists/public']:
+#     try:
+#         response = requests.get(api_url, headers=headers)
+#         response.text
+#         data = response.json()
+#         print(data)
+#     except: 
+#         print("failed")
+#     else:
+#         print(response.status_code)
